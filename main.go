014ebcde39ffc55f81149897d9d9ebe2f89ef9c2
kzbandai <nisigin@gmail.com>
2018-08-16 23:35:40 -0700

    Add some commands

--- main.go
+++ main.go
@@ -1,16 +1,55 @@
 package main
 
 import (
-	"log"
 	"os"
+	"os/exec"
 
-	"github.com/kzbandai/mirin/commands"
 	"github.com/urfave/cli"
 )
 
 var (
-	author = "kzbandai"
+	author      = "kzbandai"
+	copyright   = "MIT"
+	description = ""
+	hideHelp    = true
+	name        = "mirin"
+	usage       = "Updates manager by Go"
+	version     = "0.0.1"
 )
+
+var s []cli.Command
+
+var commands = []Definition{
+	{
+		"anyenv",
+		[]string{"update"},
+	},
+	{
+		"brew",
+		[]string{"upgrade"},
+	},
+	{
+		"composer",
+		[]string{"self-update"},
+	},
+	{
+		"gcloud",
+		[]string{"container", "update"},
+	},
+	{
+		"npm",
+		[]string{"install", "-g", "npm@latest"},
+	},
+	{
+		"yarn",
+		[]string{"self-update"},
+	},
+}
+
+type Definition struct {
+	Name string
+	Args []string
+}
 
 type Mirin struct {
 	*cli.App
@@ -24,7 +63,7 @@ func main() {
 
 	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatal(err)
+		os.Exit(1)
 	}
 }
 
@@ -34,10 +73,33 @@ func newMirin() *Mirin {
 
 func (m *Mirin) setInfo() {
 	m.Author = author
+	m.Copyright = copyright
+	m.Description = description
+	m.HideHelp = hideHelp
+	m.Name = name
+	m.Usage = usage
+	m.Version = version
 }
 
 func (m *Mirin) setCommands() {
-	m.Commands = []cli.Command{
-		commands.GetHomebrew(),
+	for _, d := range commands {
+		path, err := exec.LookPath(d.Name)
+		if err != nil {
+
+		}
+
+		s = append(s, cli.Command{
+			Name: d.Name,
+			Action: func(c *cli.Context) error {
+				cmd := exec.Command(path, d.Args...)
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+				cmd.Start()
+
+				return cmd.Wait()
+			},
+		})
 	}
+
+	m.Commands = s
 }
